7d7af93cb5254a90a89bfc887d0f30e2f872f560
Shilpa Kancharla <shilpakancharla18@gmail.com>
2021-04-10 19:23:15 -0400

    Bidirectional LSTM

--- model.py
+++ model.py
@@ -1,12 +1,9 @@
 import numpy as np 
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM
+from keras.layers import Dense, Dropout, Bidirectional, LSTM
 from tensorflow.python.keras import regularizers
-from keras.optimizers import Adam
-from keras_radam import RAdam 
 from sklearn.metrics import classification_report
-from evaluation import f1, precision_measure, recall_measure
 
 """
     Function to create model. Required for KerasClassifier.
@@ -19,16 +16,13 @@ from evaluation import f1, precision_measure, recall_measure
     @return LSTM model
 """
 
-def define_LSTM_model(training_X, training_y, dropout_rate = 0.1, l1_value = 2**-5, l2_value = 2**-5):
+def define_LSTM_model(training_X, training_y, dropout_rate, l1_value, l2_value):
     n_timesteps, n_features, n_outputs = training_X.shape[1], training_X.shape[2], training_y.shape[1]
-    initializer = tf.keras.initializers.Orthogonal()
     model = Sequential()
-    model.add(LSTM(units = 125, kernel_initializer = initializer, kernel_regularizer = regularizers.l1_l2(l1 = l1_value, l2 = l2_value),
-                    input_shape = (n_timesteps, n_features)))
-    model.add(Dropout(rate = dropout_rate))
+    model.add(Bidirectional(LSTM(units = 75, batch_input_shape = (n_timesteps, n_features))))
     model.add(Dense(units = 75, activation = 'tanh'))
+    model.add(Dropout(rate = dropout_rate))
     model.add(Dense(units = n_outputs, activation = 'softmax'))
-    model.compile(RAdam(), loss = 'categorical_crossentropy', metrics = ['accuracy', f1, precision_measure, recall_measure])
     return model
 
 """
